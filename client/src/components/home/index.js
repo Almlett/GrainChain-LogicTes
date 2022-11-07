3123cb88d4c495c2534c821b7a1abac579626197
Almic <michel.andradenavarro@gmail.com>
2022-11-06 17:58:44 -0800

    feat(validation): Validate files in express

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Home = () => {
-	return <>Home</>;
+	const [file, setFile] = useState(null);
+
+	const handleFile = (e) => {
+		setFile(e.target.files[0]);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData();
+		formData.append('file', file);
+		fetch('http://localhost:4000/upload/', {
+			method: 'POST',
+			body: formData,
+		})
+			.then((res) => res.json())
+			.then((data) => console.log(data))
+			.catch((err) => console.log(err));
+	};
+
+	return <>Home
+		<input type="file" onChange={handleFile} />
+		<button onClick={handleSubmit}>Submit</button>
+	</>;
 };
 
 export default Home;
--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@
  */
 
 const express = require('express');
-var cors = require('cors');
+const cors = require('cors');
+const upload = require('express-fileupload');
 
 const app = express();
 app.use(
@@ -14,13 +15,58 @@ app.use(
 );
 
 const port = process.env.PORT || 4000;
-
+app.use(upload());
 app.get('/test', (req, res) => {
 	res.json({
 		success: true,
 		message: 'test',
 	});
 });
+
+//recibe file and read it
+app.post('/upload', (req, res) => {
+	if (req.files) {
+		const room = [];
+		let file = req.files.file;
+		//valiodate file type txt
+		if (file.mimetype === 'text/plain') {
+			let content = file.data.toString('utf8');
+			let lines = content.split('\r\n');
+			let room_width = 0;
+			let room_height = lines.length;
+			let validation = true;
+			lines.forEach((line) => {
+				if(line.length > 0)	{
+					if (room_width !== 0 && line.length !== room_width) {
+						validation = false;
+					}
+					room_width = line.length;
+					let data = line.split('');
+					room.push(data);
+				}
+			});
+			if (validation) {
+				console.log(room, room_width, room_height, validation);
+				res.status(200).json({
+					success: true,
+					message: 'upload complete',
+				});
+			}else{
+				res.status(500).json({
+					success: false,
+					message: 'Error: The room is not valid',
+				});
+			}
+		} else {
+			res.status(500).json({
+				success: false,
+				message: 'File type not supported',
+			});
+		}
+		
+	}
+});
+
 
 app.listen(port, () => {
 	console.log('App is listening on port ' + port);
